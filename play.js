ac37b89c330338271c389a72fedd4944f47a1f14
ASHU-code06 <ashuc306@gmail.com>
2023-06-29 14:54:23 +0530

    let and const in  javascript

--- play.js
+++ play.js
@@ -1,6 +1,6 @@
-var name = 'Max';
+const name = 'Max';
 var age = 29;
-var hasHobbies = true;
+const hasHobbies = true;
 function summarizeUser(userName,userAge,userHasHobby){
     return(
         'Name is '+ userName +', Age is '+userAge+' and the user has hobbies: '+userHasHobby
