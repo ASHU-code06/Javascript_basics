43a75b517b14c51df2b2e02a310410f6484a0d7c
ASHU-code06 <ashuc306@gmail.com>
2023-07-02 23:45:11 +0530

    demoarrayprogram

---
+++ arrayprogram.js
@@ -0,0 +1,8 @@
+ numbers = ["queue","hello","12","ashu","45"];//declared an array
+ //three elements on second index but remove nothing
+// numbers=numbers.slice(2,'w','e','r');
+//numbers.slice(2,5);
+ console.log(numbers);
+ letter = ["q","w"];
+ arraytwo = [...numbers,...letter];
+ console.log(arraytwo);
--- practiceasync.js
+++ practiceasync.js
@@ -6,7 +6,7 @@ function f2(){
 }
 function main(){
     console.log('main');
-    setTimeout(f1,0);
+    setTimeout(f1,2000);
     new Promise(function(resolve,reject){
         resolve('I am a promise');
     }).then(resolve=>console.log(resolve));
